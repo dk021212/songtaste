1a3069b5d1a111956cb70c9bcd8a0ef875daad6c
dk021212 <dk021212@sina.com>
2015-03-19 15:19:24 +0800

    songtaste

--- src/com/stone/drawer/SimpleActivity.java
+++ src/com/stone/drawer/SimpleActivity.java
@@ -1,18 +1,73 @@
 package com.stone.drawer;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import com.stone.songtaste.R;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ListView;
 
-public class SimpleActivity extends FragmentActivity {
+public class SimpleActivity extends FragmentActivity implements OnClickListener {
 	private ViewPager mViewPager;
 	private List<Fragment> mFragmentList;
-	
+	private DrawerLayout mDrawerLayout;
+	private ListView mLvMenu;
+	private Button mBtnMenu;
+
 	@Override
-	protected void onCreate(Bundle savedInstanceState){
-		
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.black_drawer_simple_activity);
+
+		mDrawerLayout = (DrawerLayout) findViewById(R.id.id_drawer);
+		mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow,
+				GravityCompat.START);
+		mBtnMenu = (Button) findViewById(R.id.id_btn_menu);
+		mBtnMenu.setOnClickListener(this);
+
+		mLvMenu = (ListView) findViewById(R.id.id_lv_menu);
+
+		initViewPager();
 	}
+
+	private void initViewPager() {
+		mViewPager = (ViewPager) findViewById(R.id.id_view);
+		mFragmentList = new ArrayList<Fragment>();
+
+		SimpleFragment fragment01 = SimpleFragment
+				.newInstance("The First One!!!");
+		mFragmentList.add(fragment01);
+		SimpleFragment fragment02 = SimpleFragment
+				.newInstance("The Second One!!!");
+		mFragmentList.add(fragment02);
+		SimpleFragment fragment03 = SimpleFragment
+				.newInstance("The Third One!!!");
+		mFragmentList.add(fragment03);
+
+		mViewPager.setAdapter(new SimpleFragmentAdapter(
+				getSupportFragmentManager(), mFragmentList));
+		mViewPager.setCurrentItem(0);
+	}
+
+	@Override
+	public void onClick(View v) {
+		if (mDrawerLayout.isDrawerOpen(mLvMenu)) {
+			mDrawerLayout.closeDrawer(mLvMenu);
+		} else {
+			mDrawerLayout.openDrawer(mLvMenu);
+		}
+
+	}
+
 }
--- src/com/stone/drawer/SimpleFragment.java
+++ src/com/stone/drawer/SimpleFragment.java
@@ -7,14 +7,30 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class SimpleFragment extends Fragment {
+
+	public final static String TEXT_VALUE = "TextValue";
+
+	static SimpleFragment newInstance(String s) {
+		SimpleFragment fragment = new SimpleFragment();
+		Bundle bundle = new Bundle();
+		bundle.putString(TEXT_VALUE, s);
+		fragment.setArguments(bundle);
+		return fragment;
+	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstance) {
+		Bundle args = getArguments();
+		String text = args != null ? args.getString(TEXT_VALUE)
+				: "Default Value";
 		View view = inflater.inflate(R.layout.black_drawer_simple_fragment,
 				container, false);
+		TextView tvView=(TextView)view.findViewById(R.id.id_text);
+		tvView.setText(text);
 		return view;
 	}
 }
